df7de6f9916dc1beefcba6d227f6785a21f04e71
justincox56601 <61029178+justincox56601@users.noreply.github.com>
2021-09-07 09:31:31 -0500

    Update config.php
    Private data has been replaced with XXXXXXX

--- introDatabases/app/config/config.php
+++ introDatabases/app/config/config.php
@@ -8,14 +8,14 @@
  define('URL_ROOT', 'http://localhost/introDatabases/');
 
  //API credentials
- define('API_CLIENT_ID', 'xBso4Jv60Sn3lSXfUPLd3d9UbgQ7UB3f');
- define('API_CLIENT_SECRET', '1YeKbwNVrtUi0lvJ');
+ define('API_CLIENT_ID', 'XXXXXXXXX');
+ define('API_CLIENT_SECRET', 'XXXXXXX');
  define('API_ROOT', 'https://test.api.amadeus.com');
  define('API_TOKEN_ENDPOINT', '/v1/security/oauth2/token');
  define('API_FLIGHT_OFFER_ENDPOINT', '/v2/shopping/flight-offers');
 
  //Database credentials
  define('DB_HOST', 'localhost');
- define('DB_USER', 'root');
- define('DB_PASS', '');
- define('DB_NAME', 'intro_to_databases');
+ define('DB_USER', 'XXXXXXX');
+ define('DB_PASS', 'XXXXXXX');
+ define('DB_NAME', 'XXXXXXX');
